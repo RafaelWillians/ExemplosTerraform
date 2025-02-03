ce2dd617613163633768067a7ef1fe2f92bf753a
Rafael Silva Willians <rafael.willians@outlook.com>
2025-02-03 18:19:58 +0000

    Retirado filtro ami para teste padrao

--- exemplos/criarInstancia/createInstance.tf
+++ exemplos/criarInstancia/createInstance.tf
@@ -2,18 +2,8 @@ provider "aws" {
     region = "us-east-1"
 }
 
-data "aws_ami" "amazon_linux" {
-    most_recent = true
-    owners = ["amazon"]
-
-    filter {
-      name = "name"
-      values = "al2023-ami-*-x86_64"]
-    }
-}
-
 resource "aws_instance" "exemplo1" {
-    ami = data.aws_ami.amazon_linux.id
+    ami = "ami-0c614dee691cbbf37"
     instance_type = "t2.micro"
 
     tags = {
